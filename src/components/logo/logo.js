5adba89260f7210472ffc1cd66c25349f81123ee
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-07-24 14:08:08 -0400

    logo update

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-//import logo from './logo.png';
+import logo from './logo.png';
 import './logo.css';
 
 const Logo = () => {
 		return(
 			<div>
-				<img className='logo' alt='logo' src='https://image.freepik.com/free-vector/biometric-person-identification-facial-recognition-concept-futuristic-low-polygonal-human-face_67515-466.jpg'/>	
+				<img className='logo' alt='logo' src={logo}/>	
 			</div>
 		);
 }
