213173266ce84a23f1205f74e5c7a87848c9419e
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-08-09 19:29:49 -0400

    updating

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Banner = () => {
 	return (
 		<div>
-			{/*<p className='bg-light-blue f5 link dim black black underline pa4 w pointer'>sign out</p>*/}
 			<p className='tagline pr2'>
 				Face detection in images
 			</p>
