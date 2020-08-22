088e1fb11e231ba9e1e8321a4577a5be5b29f6e4
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-08-22 18:47:23 -0400

    loading

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -3,8 +3,8 @@ import spinner from './spinner.gif';
 
 const Loading = () => {
 	return(
-			<div className="mt6 center">
-              <img alt="loading" src={spinner} width="250px" height="auto"/>
+			<div className="mt2 center">
+              <img alt="loading" src={spinner} width="540px" height="260px"/>
             </div>
 	);
 }
