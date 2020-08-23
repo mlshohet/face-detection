03857d907a902155bddefd3d0c78251914092f8b
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-08-22 22:38:21 -0400

    update css

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -4,7 +4,7 @@ import spinner from './spinner.gif';
 
 const Loading = () => {
 	return(
-			<div className="center mt1 pv6">
+			<div className="center mt6 mb5">
               <img className="loading" alt="load" src={spinner} />
 			</div>
 	);
