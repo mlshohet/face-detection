7bdf2012021c93732410582138ed7a6e25288789
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-08-22 20:27:11 -0400

    fixing spinner

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -2,15 +2,9 @@ import React from 'react';
 import './loading.css';
 import spinner from './spinner.gif';
 
-componentDidMount() {
-	document.body.style.overflow = 'auto';
-}
-
 const Loading = () => {
 	return(
-			<div className="mt1 center">
-              <img className="loading" alt="loading" src={spinner} />
-            </div>
+              <img className="loading mt1 center" alt="loading" src={spinner} />
 	);
 }
 
