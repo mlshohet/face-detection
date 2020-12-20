c5332367b71f9791ee0b1e27ffc832537b66faed
Mike Shohet <mlshohet@gmail.com>
2020-12-19 22:29:23 -0500

    fixed styles

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -12,7 +12,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
 			);
 		} else {
 			return (
-				<nav className='navigation pt4 bg-black f6 link black'>
+				<nav className='navigation pt4 pb4 bg-black f6 link black'>
 					<div onClick={() => onRouteChange('signout')} className='white pointer dim pr3'>SIGN IN</div>
 					<div onClick={() => onRouteChange('register')} className='white pointer dim pr3'>REGISTER</div>
 				</nav>
